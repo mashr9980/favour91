e9eb3ffd6e2931e7b4181d97177d608aae382dff
Muhammad aashir <46085731+mashr9980@users.noreply.github.com>
2025-07-29 02:05:45 +0500

    Improve mobile responsiveness

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,6 +34,11 @@ export const metadata = {
   },
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html
